228ce22284c76f01908fdafffb4373e3c1f32bfc
danelian <141317269+danelian@users.noreply.github.com>
2024-03-23 21:50:44 +0400

    install axios, add header, update routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,32 @@
 <template>
-  <pre>{{ meals }}</pre>
+  <div class="flex flex-col p-8 items-center justify-center">
+    <input 
+      type="text" 
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for Meals"
+    >
+
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
+        {{ letter }}
+      </router-link>
+    </div>
+
+    <pre>{{ ingredients }}</pre>
+  </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import store from '../store'
+import axiosClient from '../axiosClient.js'
 
-const meals = computed(() => store.state.meals)
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const ingredients = ref([])
+
+onMounted(async () => {
+  const response = await axiosClient.get('/list.php?i=list')
+  console.log(response.data)
+  ingredients.value = response.data
+})
 </script>
